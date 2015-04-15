739ce9d3b39a5087a5927470fd47aae9a3e3e274
OscarMunozBernales <munoz.bernales.oscar@gmail.com>
2015-04-15 03:17:34 -0300

    lectura del archivo access.log

--- tarea02.c
+++ tarea02.c
@@ -3,6 +3,20 @@
 #include <time.h>
 #include <string.h>
 
-int main(int argc,char *argv[]){
+void leerArchivo(char path[]){
+    FILE *archivo;
+    char line[201];
 
+    archivo = fopen(path,"r");
+    if(archivo != NULL){
+        while(!feof(archivo)){
+            //fscanf(archivo,"%s\n", &line);
+            fgets(line, 201, archivo);
+            printf("linea: %s\n",line);
+        }
+        fclose(archivo);
+    }
+}
+int main(int argc,char *argv[]){
+    leerArchivo("access.log");
 }
